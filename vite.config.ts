b0993640a28d3fe95418cf30482c4983d5dd8117
mohammednasser-32 <nasser_junior94@hotmail.com>
2026-07-11 18:59:03 +0300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
-  base: '/nasser-junior-portfolio/',
+  base: '/',
   resolve: {
     tsconfigPaths: true
   }});
